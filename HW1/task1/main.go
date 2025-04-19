d9f6fe21c43cdf3143fdde76eaceb04cabdee67c
HappyCoder <mnhev@yandex.ru>
2025-04-20 00:02:58 +0300

    Add HW1 task1

--- HW1/task1/main.go
+++ HW1/task1/main.go
@@ -1,40 +1,14 @@
 package main
 
-import (
-    "flag"
-    "fmt"
-    "os"
-)
+import "fmt"
 
 func main() {
-
-    fooCmd := flag.NewFlagSet("foo", flag.ExitOnError)
-    fooEnable := fooCmd.Bool("enable", false, "enable")
-    fooName := fooCmd.String("name", "", "name")
-
-    barCmd := flag.NewFlagSet("bar", flag.ExitOnError)
-    barLevel := barCmd.Int("level", 0, "level")
-
-    if len(os.Args) < 2 {
-        fmt.Println("expected 'foo' or 'bar' subcommands")
-        os.Exit(1)
-    }
-
-    switch os.Args[1] {
-
-    case "foo":
-        fooCmd.Parse(os.Args[2:])
-        fmt.Println("subcommand 'foo'")
-        fmt.Println("  enable:", *fooEnable)
-        fmt.Println("  name:", *fooName)
-        fmt.Println("  tail:", fooCmd.Args())
-    case "bar":
-        barCmd.Parse(os.Args[2:])
-        fmt.Println("subcommand 'bar'")
-        fmt.Println("  level:", *barLevel)
-        fmt.Println("  tail:", barCmd.Args())
-    default:
-        fmt.Println("expected 'foo' or 'bar' subcommands")
-        os.Exit(1)
-    }
+	slice := []int{1, 2, 3}
+	sLength, n := len(slice), len(slice)
+	newSize := 2 * sLength
+	newSlice := make([]int, newSize)
+	for i := 0; i < sLength; i++ {
+		newSlice[i], newSlice[i+n] = slice[i], slice[i]
+	}
+	fmt.Println(newSlice)
 }
