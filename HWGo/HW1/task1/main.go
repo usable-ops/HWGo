28c2667781431230a51f5724f1242940520d66ff
HappyCoder <mnhev@yandex.ru>
2025-06-27 11:10:35 +0300

    add homework files

---
+++ HWGo/HW1/task1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{1, 2, 3}
+	sLength, n := len(slice), len(slice)
+	newSize := 2 * sLength
+	newSlice := make([]int, newSize)
+	for i := 0; i < sLength; i++ {
+		newSlice[i], newSlice[i+n] = slice[i], slice[i]
+	}
+	fmt.Println(newSlice)
+}
